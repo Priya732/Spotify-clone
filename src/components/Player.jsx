46637547d79591ac55bec3be59482f2ec206f512
Priyabrata <sahoopriya732@gmail.com>
2024-05-08 16:31:22 +0530

    Display current time duration and song duration is completed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 import { PlayerContext } from '../context/PlayerContext'
 const Player = () => {
 
-    const {track,seekBar,seekBg,playStatus,play,pause} =useContext(PlayerContext)
+    const {track,seekBar,seekBg,playStatus,play,pause,time} =useContext(PlayerContext)
 
 
 
@@ -26,11 +26,11 @@ const Player = () => {
             <img src={assets.loop_icon} className='w-4 cursor-pointer' alt="" />
         </div>
         <div className='flex items-center gap-5'>
-            <p>1:06</p>
+            <p>{time.currentTime.minute}:{time.currentTime.second}</p>
             <div ref={seekBg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
                 <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full'/>
             </div>
-            <p>3:20</p>
+            <p>{time.totalTime.minute}:{time.totalTime.second}</p>
         </div>
       </div>
       <div className='hidden lg:flex items-center gap-2 opacity-75'>
